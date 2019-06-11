f87d92d45568958a6d3b2548431127aeaf175729
Aron Roberts <robotros1@msn.com>
2019-06-10 17:41:06 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import offline from '../img/offline.png';
-import support as '../img/support.png';
+import Support from '../img/support.png';
 
 
 /**
@@ -10,17 +9,19 @@ import support as '../img/support.png';
 class Home extends React.Component {
   state = {
   }
-
   /**
   * Render Component into html
   * @return {Component} html
   */
   render() {
+    let support=Support;
     return (
       <div className='Home container'>
-        <img src={support} alt='laptops'</img>
-        <p>We are IT support specialist with years of business experience helping users in their business workplace. From small to medium enterprise size businesses, we have been helping our users solve a multitide of issues users normally face in workplace networks. We have experience in handling everything from desktop support issues all the way through entire ERP rollouts.<p>
-        <p>We understand that IT for a business is a cost center and look to help business become more effiecent by implementing the latest technologies while still lowering their expenses related to implementation and long term support<p>
+        <img width='1080px' src={support} alt='laptops'></img>
+        <h1> About Us</h1>
+        <hr></hr>
+        <p>We are IT support specialist with years of business experience helping users in their business workplace. From small to medium enterprise size businesses, we have been helping our users solve a multitide of issues users normally face in workplace networks. We have experience in handling everything from desktop support issues all the way through entire ERP rollouts.</p>
+        <p>We understand that IT for a business is a cost center and look to help business become more effiecent by implementing the latest technologies while still lowering their expenses related to implementation and long term support</p>
       </div>
     );
   }
