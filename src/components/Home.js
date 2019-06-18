68e082a82cbc35756faf7eb01daf91a3fc2c0ccb
Aron Roberts <robotros1@msn.com>
2019-06-17 17:06:21 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import Support from '../img/support.png';
 
 
 /**
-* React Component to Render WMPQ.org home page
+* React Component to Render home page
 * @author [Aron Roberts](https://github.com/robotros)
 */
 class Home extends React.Component {
@@ -20,7 +20,7 @@ class Home extends React.Component {
         <img width='1080px' src={support} alt='laptops'></img>
         <h1> About Us</h1>
         <hr></hr>
-        <p>We are IT support specialist with years of business experience helping users in their business workplace. From small to medium enterprise size businesses, we have been helping our users solve a multitide of issues users normally face in workplace networks. We have experience in handling everything from desktop support issues all the way through entire ERP rollouts.</p>
+        <p>We are leaders in the Information Technology space with more than 10 years of experience helping users in the workplace. From small to medium enterprise size businesses, we have been helping our users solve issues and overcome unique challenges in their companies technological strategies. We have experience in handling everything from basic desktop support issues all the way through extensive ERP rollouts and data migrations.</p>
         <p>We understand that IT for a business is a cost center and look to help business become more effiecent by implementing the latest technologies while still lowering their expenses related to implementation and long term support</p>
       </div>
     );
